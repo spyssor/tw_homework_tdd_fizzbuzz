e767a2f1d8ab5a3f78be33b23134c0a2c0167a2f
huyf <spyssor@gmail.com>
2019-05-25 13:45:14 +0800

    TDD第二步：完成FizzBuzz功能代码 TDD第三步：运行测试用例

--- src/main/java/com/thoughtworks/tdd/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzGame.java
@@ -6,6 +6,56 @@ public class FizzBuzzGame {
 
     public String fizzBuzz(int num){
 
-        return null;
+        if (isMultiple(num, 3)){
+            return sayFizz();
+        } else if (isMultiple(num, 5)) {
+            return sayBuzz();
+        } else if (isMultiple(num, 7)) {
+            return sayWhizz();
+        } else if (isMultiple(num, 3) && isMultiple(num, 5)) {
+            return sayFizzBuzz();
+        } else if (isMultiple(num, 3) && isMultiple(num, 7)) {
+            return sayFizzWhizz();
+        } else if (isMultiple(num, 5) && isMultiple(num, 7)) {
+            return sayBuzzWhizz();
+        } else if (isMultiple(num, 3) && isMultiple(num, 5) && isMultiple(num, 7)) {
+            return sayFizzBuzzWhizz();
+        }
+
+        return num + "";
+    }
+
+
+
+    private String sayFizz(){
+        return "Fizz";
+    }
+
+    private String sayBuzz(){
+        return "Buzz";
+    }
+
+    private String sayWhizz(){
+        return "Whizz";
+    }
+
+    private String sayFizzBuzz() {
+        return "FizzBuzz";
+    }
+
+    private String sayFizzWhizz() {
+        return "FizzWhizz";
+    }
+
+    private String sayBuzzWhizz() {
+        return "FizzBuzz";
+    }
+
+    private String sayFizzBuzzWhizz(){
+        return "FizzBuzzWhizz";
+    }
+
+    private boolean isMultiple(int num, int divisor){
+        return num % divisor == 0;
     }
 }
